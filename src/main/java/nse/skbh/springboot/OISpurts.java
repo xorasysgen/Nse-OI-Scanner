e9779298ad4eb0062e560d18cea3c2c37c36caa9
Sushil K Bhaskar <Sushil K Bhaskar@OptimusPrime>
2018-05-03 15:08:24 +0530

    OI Spurts building module version 1.0.0.1 with OS resource

---
+++ src/main/java/nse/skbh/springboot/OISpurts.java
@@ -0,0 +1,17 @@
+package nse.skbh.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OISpurts {
+	
+	
+	@RequestMapping("OS")
+	public String showOISpurts() {
+		return "OI Spurts building module version 1.0.0.1";
+		
+	}
+	
+
+}
