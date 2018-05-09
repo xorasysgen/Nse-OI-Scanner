6816b9e22fd5bd58a13e91f3f7b82aa8a86065d0
Sushil K Bhaskar <Sushil K Bhaskar@OptimusPrime>
2018-05-09 13:18:06 +0530

    @RestController  replaced with @Controller

--- src/main/java/nse/skbh/springboot/NseBoot.java
+++ src/main/java/nse/skbh/springboot/NseBoot.java
@@ -1,14 +1,12 @@
 package nse.skbh.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
+@Controller
 @SpringBootApplication
 public class NseBoot {
 
