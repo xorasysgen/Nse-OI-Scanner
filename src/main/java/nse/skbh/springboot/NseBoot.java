38a306a42e09e69d427230423b56a150eef63d2c
Sushil K Bhaskar <Sushil K Bhaskar@OptimusPrime>
2018-05-09 13:50:03 +0530

    @restController added

--- src/main/java/nse/skbh/springboot/NseBoot.java
+++ src/main/java/nse/skbh/springboot/NseBoot.java
@@ -2,11 +2,11 @@ package nse.skbh.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class NseBoot {
 
--- src/main/java/nse/skbh/springboot/OISpurts.java
+++ src/main/java/nse/skbh/springboot/OISpurts.java
@@ -2,15 +2,13 @@ package nse.skbh.springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OISpurts {
 	
 	
-	@RequestMapping("os")
+	@RequestMapping("/os")
 	public String showOISpurts() {
-		RestTemplate restTemplate = new RestTemplate();
 		return "OI Spurts building module version 1.0.0.1 system : diffrent than git";
 		
 	}
