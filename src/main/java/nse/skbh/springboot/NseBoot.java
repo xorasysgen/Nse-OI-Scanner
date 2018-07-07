cc219071aa3b2dd5ab6ace780e6e60dbea5614f6
JSR101 <JSR101@SUSHILJSR101>
2018-07-07 15:11:09 +0530

    @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})

--- src/main/java/nse/skbh/springboot/NseBoot.java
+++ src/main/java/nse/skbh/springboot/NseBoot.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import nse.skbh.springboot.pojo.ServicesList;
 @RestController
 @SpringBootApplication
 @CrossOrigin
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class NseBoot {
 
 	@RequestMapping("/")
