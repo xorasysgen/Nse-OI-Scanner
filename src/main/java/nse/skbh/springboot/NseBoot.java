1bb33553c07a316403c0673c852858bf5f4fcb1e
Sushil K Bhaskar <Sushil K Bhaskar@OptimusPrime>
2019-01-25 10:27:54 +0530

    Application shutting down and Context will be Reloaded in 10 seconds...

--- src/main/java/nse/skbh/springboot/NseBoot.java
+++ src/main/java/nse/skbh/springboot/NseBoot.java
@@ -3,8 +3,12 @@ package nse.skbh.springboot;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
 import nse.skbh.springboot.pojo.ServerStatus;
 import nse.skbh.springboot.pojo.Services;
 import nse.skbh.springboot.pojo.ServicesList;
@@ -22,7 +25,10 @@ import nse.skbh.springboot.pojo.ServicesList;
 @SpringBootApplication
 @CrossOrigin
 public class NseBoot {
-
+	
+	private static ConfigurableApplicationContext context;
+	private static Logger logger = LoggerFactory.getLogger(NseBoot.class);
+	
 	@RequestMapping("/")
 	@ResponseBody
 	ServerStatus root() {
@@ -46,11 +52,12 @@ public class NseBoot {
 		return servicesObj;
 	}
 
+	
+		
 	public static void main(String[] args) {
-
-		SpringApplication.run(NseBoot.class, args);
+		context =SpringApplication.run(NseBoot.class, args);
 		// boot from here, run as simple java program, rest it will take care of
-		// it. config file
+		// it. config files
 
 	}
 
@@ -63,4 +70,23 @@ public class NseBoot {
 		return resolver;
 	}
 
+	
+	public static void restart() {
+        ApplicationArguments args = context.getBean(ApplicationArguments.class);
+ 
+        Thread thread = new Thread(() -> {
+        	logger.warn("Application shutting down and Context will be Reloaded in 10 seconds...");
+            try {
+            	Thread.sleep(10000);// hold system for 5 seconds
+           
+			} catch (InterruptedException exceptionObject) {
+				exceptionObject.printStackTrace();
+			}
+            context.close();
+            context = SpringApplication.run(NseBoot.class, args.getSourceArgs());
+        });
+ 
+        thread.setDaemon(false);
+        thread.start();
+    }
 }
